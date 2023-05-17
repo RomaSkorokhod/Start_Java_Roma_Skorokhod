77b23b6b57543b3634a1d069fd68c1ad8d29ee56
Roma <roma.sk.ukr@gmail.com>
2023-05-17 23:39:59 +0300

    new

---
+++ src/lesson2/Practice.java
@@ -0,0 +1,41 @@
+package lesson2;
+
+public class Practice {
+    /* public static void main(String[] args) {
+         int a = 3;
+         int b = 8;
+         int t = 5;
+         int resultOfSum = a + b + t;
+         int resultOfx = a * (b - t);
+         System.out.println(resultOfSum);
+         System.out.println(resultOfx);
+   public static void main(String[] args) {
+       int x, y, r, e;
+       x = 3;
+       y = 7;
+       r = 9;
+       e = x + y;
+       System.out.println(e);
+       System.out.println(r-x);
+   }
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        a = a + b; //16
+        b = a - b; //7
+        a = a - b; //9
+        System.out.println(a);
+        System.out.println(b);*/
+
+    public static void main(String[] args) {
+
+        double t = 4.3;
+        int u = 9;
+        float p = 2.25F;
+        float e = 2.75F;
+        System.out.println(p+e+t-u);
+
+
+
+    }
+}
