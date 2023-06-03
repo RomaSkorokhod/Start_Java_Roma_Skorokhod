6670298e637325aa3d45cf3bead8606189afae85
Roma <roma.sk.ukr@gmail.com>
2023-06-03 19:02:18 +0300

    new1

--- src/lesson2/ОстатокОтДеления.java
+++ src/lesson2/ОстатокОтДеления.java
@@ -4,6 +4,6 @@ public class ОстатокОтДеления {
     public static void main(String[] args) {
         int x = 2;
         int y = 7;
-        System.out.println(x%y);
+        System.out.println(y%x);
     }
 }
