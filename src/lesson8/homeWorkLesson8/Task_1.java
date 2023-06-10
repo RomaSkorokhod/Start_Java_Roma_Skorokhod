c50e445e81aee2b243c9cf6d85c92f1696ccb1be
General <roma.sk.ukr@gmail.com>
2023-06-10 14:02:54 +0300

    new1

--- src/lesson8/homeWorkLesson8/Task_1.java
+++ src/lesson8/homeWorkLesson8/Task_1.java
@@ -1,11 +1,24 @@
 package lesson8.homeWorkLesson8;
 
-public class Task_1{
+public class Task_1 {
 /*Написать метод, принимающий в качестве параметра массив целых чисел.
 И выводит на экран все четные числа списком,
 а также нечетные числа списком.*/
 
+    public static void main(String[] args) {
+        int[] numbers = {2, 23, 5, 11, 7, 9};
 
-
-
+        System.out.print("Парні числа: ");
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.print(numbers[i] + ", ");
+            }
+            System.out.print("Непарні числа: ");
+            for (i = 0; i < numbers.length; i++) {
+                if (numbers[i] % 2 != 0) {
+                    System.out.print(numbers[i] + ", ");
+                }
+            }
+        }
+    }
 }
--- src/lesson8/homeWorkLesson8/Task_2.java
+++ src/lesson8/homeWorkLesson8/Task_2.java
@@ -12,4 +12,40 @@ public class Task_2 {
          - Принимать в качестве параметров число и строку, тогда он будет выводить на консоль сообщение типа:
             "Ваше сообщение - "%%%%%%%%", ваше число -  $",
     где "%%%%%%%%" и $ ваши введенные строка и число соответственно.*/
+
+    public static void doSmth() {
+        System.out.println("Я пустой");
+    }
+
+    public static void doSmth(String text) {
+        System.out.println("" + text);
+    }
+
+    public static void doSmth(String text1, String text2, String text3){
+        System.out.println(text1 + " " + text2 + " " + text3);
+    }
+
+    public static void doSmth(int number1, int number2, int number3){
+        System.out.println(number1 + number2 + number3);
+    }
+
+    public static void doSmth(int nubmer, String text){
+        System.out.println("Ваше повідомлення - " + text + "Ваше число - " + nubmer);
+    }
+
+    public static void main(String[] args) {
+        doSmth();
+        doSmth("Привет" );
+        doSmth("Hello","my","friend");
+        doSmth(3, 5, 6);
+        doSmth(7 , "Good morning! ");
+
+
+
+
+
+
+
+    }
+
 }
