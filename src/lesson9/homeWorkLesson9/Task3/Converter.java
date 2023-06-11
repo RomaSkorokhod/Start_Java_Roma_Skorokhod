9f0fd1bf9140296af014563dc98ff05742bd7d33
General <roma.sk.ukr@gmail.com>
2023-06-11 19:05:26 +0300

    new1

--- src/lesson9/homeWorkLesson9/Task3/Converter.java
+++ src/lesson9/homeWorkLesson9/Task3/Converter.java
@@ -1,7 +1,13 @@
 package lesson9.homeWorkLesson9.Task3;
 
 public class Converter {
-    public String name;
+   public String name;
+    /* 3) Необходимо создать класс конвертер, который будет иметь методы конвертации примитивных типов данных:
+    метод convertToInt; (конвертирует byte, short, int, long, char, float, double, String). При вводе boolean выводит сообщение, что введен тип boolean.
+    convertToDouble;    (конвертирует byte, short, int, long, char, float, double, String). При вводе boolean выводит сообщение, что введен тип boolean.
+    converToString;     (конвертирует byte, short, int, long, char, boolean, float, double, String).
+    У данного класса должен быть только один конструктор, в параметрах которого можно указать его имя.
+    А также только один метод геттер для получения информации о названии данного конвертра.*/
 
     public Converter(String name) {
         this.name = name;
@@ -10,12 +16,6 @@ public class Converter {
     public String getName() {
         return name;
     }
-    /* 3) Необходимо создать класс конвертер, который будет иметь методы конвертации примитивных типов данных:
-    метод convertToInt; (конвертирует byte, short, int, long, char, float, double, String). При вводе boolean выводит сообщение, что введен тип boolean.
-    convertToDouble;    (конвертирует byte, short, int, long, char, float, double, String). При вводе boolean выводит сообщение, что введен тип boolean.
-    converToString;     (конвертирует byte, short, int, long, char, boolean, float, double, String).
-    У данного класса должен быть только один конструктор, в параметрах которого можно указать его имя.
-    А также только один метод геттер для получения информации о названии данного конвертра.*/
 
     public int convertToInt(byte b){
         return b;
@@ -38,11 +38,67 @@ public class Converter {
     public int convertToInt(double d){
         return (int) d;
     }
-    public int convertToInt(String s){
-        return Integer.parseInt(s);
+    public int convertToInt(String st){
+        return Integer.parseInt(st);
     }
-    public int convertToInt(boolean b){
-        System.out.println("введен тип boolean");
+    public int convertToInt(boolean bl){
+        System.out.println("Введен тип boolean");
         return 0;
+    }
+
+    public double convertToDouble(byte b){
+        return b;
+    }
+    public double convertToDouble(short s){
+        return s;
+    }
+    public double convertToDouble(int i){
+        return i;
+    }
+    public double convertToDouble(long l){
+        return l;
+    }
+    public double convertToDouble(char c){
+        return c;
+    }
+    public double convertToDouble(float f){
+        return f;
+    }
+    public double convertToDouble(double d){
+        return d;
+    }
+    public double convertToDouble(String st){
+        return Double.parseDouble(st);
+    }
+    public double convertToDouble(boolean bl) {
+        System.out.println("Введен тип boolean");
+        return 0;
+    }
+    public String converToString(byte b){
+        return String.valueOf(b);
+    }
+    public String converToString(short s){
+        return String.valueOf(s);
+    }
+    public String converToString(int i){
+        return String.valueOf(i);
+    }
+    public String converToString(long l){
+        return String.valueOf(l);
+    }
+    public String converToString(char c){
+        return String.valueOf(c);
+    }
+    public String converToString(float f){
+        return String.valueOf(f);
+    }
+    public String converToString(double d){
+        return String.valueOf(d);
+    }
+    public String converToString(String st){
+        return String.valueOf(st);
+    }
+    public String converToString(boolean bl){
+        return String.valueOf(bl);
     }
 }
--- src/lesson9/homeWorkLesson9/Task3/Main.java
+++ src/lesson9/homeWorkLesson9/Task3/Main.java
@@ -5,9 +5,12 @@ import lesson9.homeWorkLesson9.Task3.Converter;
 public class Main {
     public static void main(String[] args) {
         Converter converter = new Converter("Converter");
-        int x = converter.convertToInt('f');
+        int x = converter.convertToInt('g');
         int a = converter.convertToInt(false);
+        double m = converter.convertToDouble('t');
+        String w = converter.converToString("Проверка string");
         System.out.println(x);
-        System.out.println(a);
+        System.out.println(m);
+        System.out.println(w);
     }
 }
