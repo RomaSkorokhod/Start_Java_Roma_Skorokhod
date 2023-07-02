96dacaf3eb2a8ea0b6e1e449f15898ec23b45abc
Roma <roma.sk.ukr@gmail.com>
2023-07-02 10:40:25 +0300

    new

--- src/lesson1/Test.java
+++ src/lesson1/Test.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, next level");
+        System.out.println("Hello, next level in 2023");
     }
 }
